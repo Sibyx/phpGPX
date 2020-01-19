aa7ec54381ce99318aa03d571af5c61a012df747
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2020-01-19 21:02:32 +0100

    Fixed bugs and added extra asserts

--- tests/UnitTests/phpGPX/Helpers/SerializationHelperTest.php
+++ tests/UnitTests/phpGPX/Helpers/SerializationHelperTest.php
@@ -16,6 +16,7 @@ class SerializationHelperTest extends TestCase
 		$this->assertNull(SerializationHelper::integerOrNull(null));
 		$this->assertNull(SerializationHelper::integerOrNull("BLA"));
 		$this->assertInternalType("int", SerializationHelper::integerOrNull(5));
+		$this->assertInternalType("int", SerializationHelper::integerOrNull("5"));
 	}
 
 	public function testFloatOrNull()
@@ -25,12 +26,14 @@ class SerializationHelperTest extends TestCase
 		$this->assertNull(SerializationHelper::floatOrNull("BLA"));
 		$this->assertInternalType("float", SerializationHelper::floatOrNull(5.6));
 		$this->assertInternalType("float", SerializationHelper::floatOrNull(5));
+		$this->assertInternalType("float", SerializationHelper::floatOrNull("5.6"));
+		$this->assertInternalType("float", SerializationHelper::floatOrNull("5"));
 	}
 
 	public function testStringOrNull()
 	{
 		$this->assertNull(SerializationHelper::stringOrNull(null));
-		$this->assertInternalType("string", "");
-		$this->assertInternalType("string", "Bla bla");
+		$this->assertInternalType("string", SerializationHelper::stringOrNull(""));
+		$this->assertInternalType("string", SerializationHelper::stringOrNull("Bla bla"));
 	}
 }
